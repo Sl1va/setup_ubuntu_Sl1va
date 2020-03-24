71331faf5513a3f199524ce001a8ec649dc7e91f
Sl1va <emillatypov9335@gmail.com>
2020-03-25 00:10:19 +0300

    fixed

--- setup_system.sh
+++ setup_system.sh
@@ -4,3 +4,5 @@ echo 'y ' | sudo apt install gnome-tweaks
 echo 'y ' | sudo snap install sublime-text --classic
 echo 'y ' | sudo snap  install telegram-desktop --classic
 echo 'y ' | sudo snap  install code --classic
+echo 'y'  | sudo apt install gcc
+echo 'y'  | sudo apt install g++

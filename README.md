c5572aaf1248ed2d55e3cd1df1d2795efbc5517e
Sl1va <emillatypov9335@gmail.com>
2020-03-24 17:55:21 +0300

    fixed

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-wget https://github.com/Sl1va/setup_ubuntu_Sl1va/blob/master/setup_system.sh | sudo sh
+(echo 'y'  | sudo apt install git) && git clone https://github.com/Sl1va/setup_ubuntu_Sl1va && (cat setup_ubuntu_Sl1va/setup_system.sh | sudo sh)
--- setup_system.sh
+++ setup_system.sh
@@ -1,6 +1,5 @@
 echo 'Y ' | sudo apt update
 echo 'y ' | sudo apt upgrade
-echo 'y'  | sudo apt install git
 echo 'y ' | sudo apt install gnome-tweaks
 echo 'y ' | sudo snap install sublime-text --classic
 echo 'y ' | sudo snap  install telegram-desktop --classic

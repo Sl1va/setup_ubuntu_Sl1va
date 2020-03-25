280f45adf49f7c03ee7f3d0636e51eb8d08461f8
Sl1va <emillatypov9335@gmail.com>
2020-03-25 10:18:34 +0300

    fixed

--- setup_system.sh
+++ setup_system.sh
@@ -1,8 +1,14 @@
 echo 'Y ' | sudo apt update
 echo 'y ' | sudo apt upgrade
+
+# install packages
 echo 'y ' | sudo apt install gnome-tweaks
 echo 'y ' | sudo snap install sublime-text --classic
 echo 'y ' | sudo snap  install telegram-desktop --classic
 echo 'y ' | sudo snap  install code --classic
 echo 'y'  | sudo apt install gcc
 echo 'y'  | sudo apt install g++
+
+# create workspaces
+mkdir -p ~/lang/C ~/lang/C++ ~/lang/Python
+
